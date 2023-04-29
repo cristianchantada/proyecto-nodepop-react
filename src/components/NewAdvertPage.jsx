c7bf3b466f0e79bf3936ea7f5c3bd52bf0ebac59
Cristian Varela Casas <cristianchantada@gmail.com>
2023-04-29 12:26:32 +0200

    Consigo controlar todos los inputs del formulario advert/new y la modificación correcta del estado

--- src/components/NewAdvertPage.jsx
+++ src/components/NewAdvertPage.jsx
@@ -1,68 +1,72 @@
 import { useNavigate } from "react-router-dom";
-import Layout from "./common/Layout";
 import { postAdv } from "../api/service";
+import Layout from "./common/Layout";
 import { useState } from "react";
 
 function NewAdvertPage({handleLogout, isLogged}) {
 
-  const[inputValue, setInputValue] = useState('');
+  const[advData, setAdvData] = useState({
+    name: '',
+    sale: false,
+    price: '',
+    tags: {
+      lifestyle: false,
+      work: false,
+      motor: false,
+      mobile: false
+    },
+    photo: ''
+  });
+
+  console.log(advData.tags);
+
   const navigate = useNavigate();
 
-  const handleNewAdvertButton = () => {
-    navigate('/adverts/new');
-  }
-
   const handleChange = (event) => {
-    setInputValue(event.target.value);
 
-    console.log(event.target.value);
-    console.log(event.target.checked);
+    if(event.target.name === 'name'){
+      setAdvData({...advData, name: event.target.value})
+    }
+    if(event.target.name === 'sale'){
+      setAdvData({...advData, sale: event.target.value})
+    }
+    if(event.target.name === 'price'){
+      setAdvData({...advData, price: event.target.value})
+    }
+    if(event.target.name === 'photo'){
+      setAdvData({...advData, photo: event.target.photo.files[0]})
+    }
+    if(event.target.name === 'lifestyle' || event.target.name === 'motor' || event.target.name === 'work' || event.target.name === 'mobile'){
+ 
+      setAdvData({...advData, tags: {...advData.tags, [event.target.name]: event.target.checked}})
+
+     }
   }
 
   const handleSubmit = event => {
     event.preventDefault();
-
-    let sale = event.target[1].value;
-    sale = sale === 'vender' ? true : false;
-
-    let tagsArray = [];
-    for(let i=2; i<6; i++){
-      if(event.target[i].checked === true){
-        tagsArray.push(event.target[i].name);
-      }
-    }
-
-    const advData = {
-      name: event.target.name.value,
-      sale: sale,
-      price: parseFloat(event.target.prize.value),
-      tags: [tagsArray],
-      photo: event.target.photo.files[0]
-    }
-
     postAdv(advData).then(response => navigate(`/adverts/${response.id}`));
-    
   }
 
   return (
     <Layout isLogged={isLogged} handleLogout={handleLogout}>
       <div className="container newAdvertContainer">
         <h2>Crear nuevo anuncio</h2>
-        <form onSubmit={handleSubmit} /* enctype="multipart/form-data" */>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="name">Nombre del producto</label>
-            <input value={inputValue} onChange={handleChange} type="text" name='name' id="name" required />
+            <input value={advData.name} onChange={handleChange} type="text" name='name' id="name" required />
           <label htmlFor="sale">Seleccione compra o venta</label>
-            <select onChange={handleChange} name="sale" id="sale" required>
-              <option value="comprar">Comprar</option>
-              <option value="vender">Vender</option>
+            <select value={advData.sale} onChange={handleChange} name="sale" id="sale" required>
+              <option value={false}>Comprar</option>
+              <option value={true}>Vender</option>
             </select>
           <label htmlFor="tags">Tags</label>
-            <label htmlFor="lifestyle" id='lifestyle' name='lifestyle' ><input onChange={handleChange} type="checkbox" name="lifestyle" id="lifestyle" />Lifestyle</label>
-            <label htmlFor="work" id='work' name='work' ><input onChange={handleChange} type="checkbox" name="work" id="work" />Work</label>
-            <label htmlFor="motor" id='motor' name='motor' ><input onChange={handleChange} type="checkbox" name="motor" id="motor" />Motor</label>
-            <label htmlFor="mobile" id='mobile' name='mobile' ><input onChange={handleChange} type="checkbox" name="mobile" id="mobile" />Mobile</label>
-          <label htmlFor="prize">
-            <input onChange={handleChange} type="text" name="prize" id="prize" required/>
+            <label htmlFor="lifestyle" id='lifestyle' name='lifestyle' ><input checked={advData.tags.lifestyle} onChange={handleChange} type="checkbox" name="lifestyle" id="lifestyle" />Lifestyle</label>
+            <label htmlFor="work" id='work' name='work' ><input checked={advData.tags.work} onChange={handleChange} type="checkbox" name="work" id="work" />Work</label>
+            <label htmlFor="motor" id='motor' name='motor' ><input checked={advData.tags.motor} onChange={handleChange} type="checkbox" name="motor" id="motor" />Motor</label>
+            <label htmlFor="mobile" id='mobile' name='mobile' ><input checked={advData.tags.mobile} onChange={handleChange} type="checkbox" name="mobile" id="mobile" />Mobile</label>
+          <label htmlFor="price">
+            <input value={advData.price} onChange={handleChange} type="text" name="price" id="price" required/>
           </label>
           <label htmlFor="photo">Fotografía</label>
             <input type="file" name="photo" id="photo" />
